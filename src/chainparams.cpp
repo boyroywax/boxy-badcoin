1311285b4b1a66721d248b9544ccedbd06290830
boyroywax <jmsdevln@gmail.com>
2021-01-25 14:37:05 -0800

    switched to old pszTimestamp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -52,8 +52,8 @@ static CBlock CreateGenesisBlock(const CScript& genesisInputScript, const CScrip
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, const char* pszTimestamp="On 01/24/2021 Boxycoin was reborn.")
 {
-    const char* pszTimestamp = "BOXY is Reborn 01/24/2021";
-    // const CScript genesisInputScript = CScript() << 486604799 << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+    // const char* pszTimestamp = "BOXY is Reborn 01/24/2021";
+    const CScript genesisInputScript = CScript() << 486604799 << CScriptNum(4) << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
     const CScript genesisOutputScript = CScript() << ParseHex("09108afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, 1, 0);
 }
